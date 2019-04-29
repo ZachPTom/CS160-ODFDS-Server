a24b8d257fb18d43260195d601fd3a3d84e490b4
Kevin <guangyu.yuan@sjsu.edu>
2019-04-29 02:05:56 -0700

    address

--- ODFDS_server/api/models.py
+++ ODFDS_server/api/models.py
@@ -16,7 +16,7 @@ class Driver(models.Model):
     driver_long = models.FloatField(default=None, validators=[
         MaxValueValidator(180), MinValueValidator(-180)])
     occupied = models.BooleanField(default=False)
-    address = models.CharField()
+    address = models.CharField(max_length=100, default="")
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
